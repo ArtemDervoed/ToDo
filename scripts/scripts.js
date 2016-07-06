6c5bc0fa8f05f7a40e1f3463ff1b4ced0a174b91
Artem <unnamed_9595@mail.ru>
2016-07-06 11:26:32 +0400

    aded remow on click

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,50 @@
 window.onload = function(){
-	localStorage.clear();
-	var counter= 0;
+	//localStorage.clear();
+	var counter= localStorage.length;
 	var memo = new DataStorage();
 	var node = document.getElementsByClassName("strips");
+	var todos = document.getElementsByClassName("strips--single-data");
 	var message = {
 		id : "",
 		data : "",
 		active : true
 	}
-	document.getElementsByClassName("strips")[0].onclick = function( e ) {
-		var target = e ? e.target : event.srcElement;
-		while ( target != this && target.nodeName.toLowerCase() != "li"  ) {
-			target = target.parentNode;
-		}
-		if ( target == this ) { 
-			return; 
-		}
-		var index = 0;
-		while ( (target = target.previousSibling) ) {
-			if ( target.nodeType === 1 ) {
-				index++;
+	var displayMode;
+	refresh(memo);
+	node[0].onclick = function( e ) {
+		var target = event.target;
+		if (target.className != 'single-data--del'){
+			return;
+		} 
+		target = target.parentNode;
+		console.log(target);
+		for(var i = 0; i < todos.length; i++){
+			if (todos[i]===target){
+				memo.remove(i);
 			}
 		}
-		counter--;
-		memo.remove(index);
+		if(displayMode === 0){
+	    	refresh(memo);
+	    }
+	    else if(displayMode === 1){
+	    	onlyDisable(memo);
+	    }
+	    else if(displayMode === 2){
+	    	onlyActive(memo);
+	    }
+		counter = localStorage.length;
+	};
+	document.getElementsByClassName("only-enable")[0].onclick = function(){
+		onlyActive(memo);
+		displayMode = 2;
+	};
+	document.getElementsByClassName("only-disable")[0].onclick = function(){
+		onlyDisable(memo);
+		displayMode = 1;
+	};
+	document.getElementsByClassName("all")[0].onclick = function(){
 		refresh(memo);
+		displayMode = 0;
 	};
 	document.onkeydown = function checkKeycode(event){
 	    if(!event){
@@ -38,12 +58,19 @@ window.onload = function(){
 	    		memo.add(counter, message);
 	    		console.log(localStorage);
 	    		textBox[0].value = "";
-	    		counter++;
-	    		refresh(memo);
+	    		counter = localStorage.length;
+	    		if(displayMode === 0){
+	    			refresh(memo);
+	    		}
+	    		else if(displayMode=== 1) {
+	    			onlyDisable(memo);
+	    		}
+	    		else if(displayMode === 2){
+	    			onlyActive(memo);
+	    		}
 	    	}
 		}
 	}
-
 }
 function switchStatus(index,memo){
 	memo.setStatusItem(index);
@@ -54,12 +81,53 @@ function switchStatus(index,memo){
 		singleStrip[index].classList.remove("__is-not-active");
 	}
 }
-function refresh(memo){
-	var node = document.getElementsByClassName('strips');
+function invalidate(){
+	node = document.getElementsByClassName('strips');
 	childs = document.getElementsByClassName('strips--single-data');
 	while(childs.length !== 0){
 		node[0].removeChild(childs[0])
+	}	
+}
+function onlyActive(memo){
+	invalidate();
+	for( var i = 0; i < memo.getLengthStorage(); i++){
+		var strip = document.createElement('li');
+		if(memo.getItem(i).active===true){
+			strip.className ='strips--single-data';		 
+			strip.innerHTML = memo.getItem(i).data;
+			var del = document.createElement('div');
+			del.className ='single-data--del';
+			del.innerHTML ="";
+			strip.appendChild(del);
+			var status = document.createElement('div');
+			status.className ='single-data--status';
+			status.innerHTML ="";
+			strip.appendChild(status);
+			node[0].insertBefore(strip,node.firstChild);
+		}
 	}
+}
+function onlyDisable(memo){
+	invalidate();
+	for( var i = 0; i < memo.getLengthStorage(); i++){
+		var strip = document.createElement('li');
+		if(memo.getItem(i).active===false){
+			strip.className ='strips--single-data __is-not-active';		 
+			strip.innerHTML = memo.getItem(i).data;
+			var del = document.createElement('div');
+			del.className ='single-data--del';
+			del.innerHTML ="";
+			strip.appendChild(del);
+			var status = document.createElement('div');
+			status.className ='single-data--status';
+			status.innerHTML ="";
+			strip.appendChild(status);
+			node[0].insertBefore(strip,node.firstChild);
+		}
+	}
+}
+function refresh(memo){
+	invalidate();
 	for( var i = 0; i < memo.getLengthStorage(); i++){
 		var strip = document.createElement('li');
 		if(memo.getItem(i).active===false){
