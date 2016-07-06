da94987a1d793829507c54922db4f6debae15f11
Artem <unnamed_9595@mail.ru>
2016-07-06 13:14:53 +0400

    completed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,36 @@
 window.onload = function(){
-	//localStorage.clear();
-	var counter= localStorage.length;
 	var memo = new DataStorage();
 	var node = document.getElementsByClassName("strips");
 	var todos = document.getElementsByClassName("strips--single-data");
+	var todosCount = document.getElementById("todos-count");
+	var counter = memo.getLengthStorage();
+	todosCount.innerHTML = "Количество заметок " +  memo.getLengthStorage(); 
 	var message = {
 		id : "",
 		data : "",
 		active : true
 	}
-	var displayMode;
+	var displayMode = 0;
 	refresh(memo);
 	node[0].onclick = function( e ) {
-		var target = event.target;
-		if (target.className != 'single-data--del'){
+		var target = e.target;
+		if (target.className !== 'single-data--del' && target.className !== 'single-data--status'){
 			return;
-		} 
-		target = target.parentNode;
-		console.log(target);
-		for(var i = 0; i < todos.length; i++){
-			if (todos[i]===target){
-				memo.remove(i);
+		}
+		if(target.className == "single-data--del"){
+			target = target.parentNode;
+			for(var i = 0; i < todos.length; i++){
+				if (todos[i]===target){
+					memo.remove(i);
+				}
+			}
+		}
+		if(target.className == "single-data--status"){
+			target = target.parentNode;
+			for(var i = 0; i < todos.length; i++){
+				if (todos[i] === target){
+					memo.setStatusItem(i);
+				}
 			}
 		}
 		if(displayMode === 0){
@@ -32,6 +42,7 @@ window.onload = function(){
 	    else if(displayMode === 2){
 	    	onlyActive(memo);
 	    }
+	    todosCount.innerHTML = "Количество заметок " + memo.getLengthStorage();
 		counter = localStorage.length;
 	};
 	document.getElementsByClassName("only-enable")[0].onclick = function(){
@@ -50,9 +61,9 @@ window.onload = function(){
 	    if(!event){
 	    	var event = window.event;
 	    }
-	    else if(event.which==13) {
+	    else if(event.which == 13) {
 	    	var textBox = document.getElementsByClassName("input-box--input");
-	    	if(textBox[0].value!=="") {
+	    	if(textBox[0].value !== "") {
 	    		message.data = textBox[0].value;
 	    		message.id = counter; 
 	    		memo.add(counter, message);
@@ -62,7 +73,7 @@ window.onload = function(){
 	    		if(displayMode === 0){
 	    			refresh(memo);
 	    		}
-	    		else if(displayMode=== 1) {
+	    		else if(displayMode === 1) {
 	    			onlyDisable(memo);
 	    		}
 	    		else if(displayMode === 2){
@@ -70,81 +81,78 @@ window.onload = function(){
 	    		}
 	    	}
 		}
+		todosCount.innerHTML = "Количество заметок " + memo.getLengthStorage();
 	}
-}
-function switchStatus(index,memo){
-	memo.setStatusItem(index);
-	var singleStrip = document.getElementsByClassName('strips--single-data');	
-	if(memo.getItem(index).active===false){
-		singleStrip[index].classList.add("__is-not-active");
-	} else if(memo.getItem(index).active===true){
-		singleStrip[index].classList.remove("__is-not-active");
+	function switchStatus(index,memo){
+		memo.setStatusItem(index);	
+		if(memo.getItem(index).active === false){
+			todos[index].classList.add("__is-not-active");
+		} else if(memo.getItem(index).active === true){
+			todos[index].classList.remove("__is-not-active");
+		}
 	}
-}
-function invalidate(){
-	node = document.getElementsByClassName('strips');
-	childs = document.getElementsByClassName('strips--single-data');
-	while(childs.length !== 0){
-		node[0].removeChild(childs[0])
-	}	
-}
-function onlyActive(memo){
-	invalidate();
-	for( var i = 0; i < memo.getLengthStorage(); i++){
-		var strip = document.createElement('li');
-		if(memo.getItem(i).active===true){
-			strip.className ='strips--single-data';		 
+	function invalidate(){
+		while(todos.length !== 0){
+			node[0].removeChild(todos[0])
+		}	
+	}
+	function onlyActive(memo){
+		invalidate();
+		for( var i = 0; i < memo.getLengthStorage(); i++){
+			var strip = document.createElement('li');
+			if(memo.getItem(i).active === true){
+				strip.className ='strips--single-data';		 
+				strip.innerHTML = memo.getItem(i).data;
+				var del = document.createElement('div');
+				del.className = 'single-data--del';
+				del.innerHTML = "";
+				strip.appendChild(del);
+				var status = document.createElement('div');
+				status.className ='single-data--status';
+				status.innerHTML ="";
+				strip.appendChild(status);
+				node[0].insertBefore(strip,node.firstChild);
+			}
+		}
+	}
+	function onlyDisable(memo){
+		invalidate();
+		for( var i = 0; i < memo.getLengthStorage(); i++){
+			var strip = document.createElement('li');
+			if(memo.getItem(i).active===false){
+				strip.className ='strips--single-data __is-not-active';		 
+				strip.innerHTML = memo.getItem(i).data;
+				var del = document.createElement('div');
+				del.className = 'single-data--del';
+				del.innerHTML = "";
+				strip.appendChild(del);
+				var status = document.createElement('div');
+				status.className = 'single-data--status';
+				status.innerHTML = "";
+				strip.appendChild(status);
+				node[0].insertBefore(strip,node.firstChild);
+			}
+		}
+	}
+	function refresh(memo){
+		invalidate();
+		for( var i = 0; i < memo.getLengthStorage(); i++){
+			var strip = document.createElement('li');
+			if(memo.getItem(i).active === false){
+				strip.className = 'strips--single-data __is-not-active';		 
+			} else {
+				strip.className = 'strips--single-data';
+			}
 			strip.innerHTML = memo.getItem(i).data;
 			var del = document.createElement('div');
 			del.className ='single-data--del';
 			del.innerHTML ="";
 			strip.appendChild(del);
 			var status = document.createElement('div');
-			status.className ='single-data--status';
-			status.innerHTML ="";
+			status.className = 'single-data--status';
+			status.innerHTML = "";
 			strip.appendChild(status);
 			node[0].insertBefore(strip,node.firstChild);
 		}
-	}
-}
-function onlyDisable(memo){
-	invalidate();
-	for( var i = 0; i < memo.getLengthStorage(); i++){
-		var strip = document.createElement('li');
-		if(memo.getItem(i).active===false){
-			strip.className ='strips--single-data __is-not-active';		 
-			strip.innerHTML = memo.getItem(i).data;
-			var del = document.createElement('div');
-			del.className ='single-data--del';
-			del.innerHTML ="";
-			strip.appendChild(del);
-			var status = document.createElement('div');
-			status.className ='single-data--status';
-			status.innerHTML ="";
-			strip.appendChild(status);
-			node[0].insertBefore(strip,node.firstChild);
-		}
-	}
-}
-function refresh(memo){
-	invalidate();
-	for( var i = 0; i < memo.getLengthStorage(); i++){
-		var strip = document.createElement('li');
-		if(memo.getItem(i).active===false){
-			strip.className ='strips--single-data __is-not-active';		 
-		} else {
-			strip.className ='strips--single-data';
-		}
-		strip.innerHTML = memo.getItem(i).data;
-		var listStrips = document.getElementsByClassName('strips');
-		var del = document.createElement('div');
-		del.className ='single-data--del';
-		del.innerHTML ="";
-		strip.appendChild(del);
-		var status = document.createElement('div');
-		status.className ='single-data--status';
-		status.innerHTML ="";
-		strip.appendChild(status);
-		listStrips[0].insertBefore(strip,listStrips.firstChild);
 	}
 }
